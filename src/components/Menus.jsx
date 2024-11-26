6c1f05960dc31b245ca8668bf480966b61a2aa73
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-26 16:24:40 -0300

    Inclusão de texto

---
+++ src/components/Menus.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+
+const MenusData = [
+  {
+    id: 1,
+    title: "Fresh Red Apples",
+    link: "/",
+    price: "$3.99"
+  },
+  {
+    id: 1,
+    title: "Fresh Oranges",
+    link: "/",
+    price: "$4.99"
+  },
+  {
+    id: 1,
+    title: "Fresh Avocado",
+    link: "/",
+    price: "$5.99"
+  },
+  {
+    id: 1,
+    title: "Fresh Cherries",
+    link: "/",
+    price: "$2.99"
+  }
+]
+
+const Menus = () => {
+  return (
+    <div>Menus</div>
+  )
+}
+
+export default Menus
