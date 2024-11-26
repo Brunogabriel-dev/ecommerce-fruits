56cfcef517ad7ee74a6ef877733405e3b6522c27
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-25 21:30:55 -0300

    Inclusão de texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+import ResponsiveMenu from "./components/ResponsiveMenu"
 
 const App = () => {
   return (
     <main className="overflow-x-hidden">
       <Navbar />
+      <ResponsiveMenu />
     </main>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { FaLeaf } from "react-icons/fa";
+import { MdMenu, MdOutlineShoppingCart } from "react-icons/md";
 
 const NavbarMenu = [
   {
@@ -43,11 +44,20 @@ const Navbar = () => {
     <div className="hidden md:block">
       <ul className="flex items-center gap-6 text-gray-600">
         {NavbarMenu.map((menu) => (
-          <li key={menu.id} className="text-xl">
-            <a href={menu.link}>{menu.title}</a>
+          <li key={menu.id}>
+            <a href={menu.link}
+            className="inline-block py-1 px-3 hover:text-primary hover:shadow-[0_3px_0_-1px_ #ef4444] font-semibold"
+            >{menu.title}</a>
           </li>
         ))}
+        <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
+          <MdOutlineShoppingCart />
+        </button>
       </ul>
+      </div>
+      {/* Mobile Hamburger Menu section*/}
+      <div className="md:hidden">
+        <MdMenu className="text-4xl" />
       </div>
     </div>
     </nav>
