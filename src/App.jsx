49e313d5cb90da1343de743952129e1e97e995a9
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-26 08:11:20 -0300

    Inclusão de texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import ResponsiveMenu from "./components/ResponsiveMenu"
+import Hero from "./components/Hero"
 
 const App = () => {
   return (
     <main className="overflow-x-hidden">
       <Navbar />
       <ResponsiveMenu />
+      <Hero />
     </main>
   )
 }
