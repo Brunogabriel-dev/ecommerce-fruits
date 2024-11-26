7b885d785e8856c28fd1d1f18b68ff8792631ded
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-26 08:31:08 -0300

    Inclusão de texto

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,24 @@ import React from "react"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section>
+      <div className="container">
+        {/* Brand Info */}
+        <div>
+          <div>
+            <h1>Healthy</h1>
+            <br />
+            <h1>
+              Fresh <span
+              className="text-secondary">Fruits!</span>{""}
+            </h1>
+          </div>
+        </div>
+
+
+        {/* Brand Images */}
+      </div>
+    </section>
   )
 }
 
