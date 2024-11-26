33684a48d46eff33c672eb93d5366493b492edae
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-26 08:55:41 -0300

    Inclusão de texto

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,11 @@ import React from "react"
 const Hero = () => {
   return (
     <section>
-      <div className="container">
+      <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px]">
         {/* Brand Info */}
-        <div>
-          <div>
-            <h1>Healthy</h1>
+        <div className="flex flex-col justify-center py-14 md:py-0 relative z-10">
+          <div className="text-center md:text-left space-y-6 lg:max-w-[400px]">
+            <h1 className="text-5xl lg:text-6xl font-bold leading-relaxed xl:leading-loose font-averia">Healthy</h1>
             <br />
             <h1>
               Fresh <span
@@ -15,8 +15,6 @@ const Hero = () => {
             </h1>
           </div>
         </div>
-
-
         {/* Brand Images */}
       </div>
     </section>
